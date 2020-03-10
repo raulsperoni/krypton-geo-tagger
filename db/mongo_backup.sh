945d79db9774ec35c3c275c6ed26988974091378
raúl <rsperoni@magnesium.coop>
2020-03-10 15:26:22 -0300

    ready

--- db/mongo_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#mongodump --host ds019766.mlab.com --username krypton-collector --password 23l5izVPF92OJm8x --port 19766 --db krypton --gzip
-
-mongodump --host $MONGO_GEO_HOST --gzip
-
-
-
-
